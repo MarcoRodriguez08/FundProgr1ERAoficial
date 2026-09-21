cf8f39216af6260a7edba9064a17299a8b296133
MarcoRodriguez08 <L26550224@chihuahua2.TECNM.MX>
2026-09-21 15:54:47 -0600

    Add files via upload

---
+++ EVA1_2HOLAMUNDO/src/main/java/com/mycompany/eva1_2holamundo/EVA1_2HOLAMUNDO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2holamundo;
+
+/**
+ *
+ * @author Daniel
+ */
+public class EVA1_2HOLAMUNDO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
