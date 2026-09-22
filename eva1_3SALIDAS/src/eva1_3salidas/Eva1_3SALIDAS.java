be709d7b32b5bb087139147d0c0c2260eb5b4026
MarcoRodriguez08 <L26550224@chihuahua2.TECNM.MX>
2026-09-21 18:07:28 -0600

    Add files via upload

---
+++ eva1_3SALIDAS/src/eva1_3salidas/Eva1_3SALIDAS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_3SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenido a fundamentos de programación");
+        System.out.println("Marco Antonio Rodríguez Sánchez");
+        System.out.print("Tu calificación es:");
+        System.out.println(79);
+    }
+    
+}
